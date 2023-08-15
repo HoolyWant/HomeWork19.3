6f17ea189e0f360696ae889d7accd396a587110b
hoolywant <sir.saltickow@yandex.ru>
2023-08-16 00:33:42 +0300

    admin interface for models is done

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from shop.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'buy_cost', 'category_name',)
+    list_filter = ('category_name',)
+    search_fields = ('product_name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
