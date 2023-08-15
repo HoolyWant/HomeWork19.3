960ba95b75c848ae32db18b58a70a959b1810687
hoolywant <sir.saltickow@yandex.ru>
2023-08-16 00:41:59 +0300

    added nullable values

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
 
 class Product(models.Model):
     product_name = models.CharField(max_length=150, verbose_name='продукт')
     description = models.CharField(max_length=150, verbose_name='описание')
-    preview = models.ImageField(upload_to='products/', verbose_name='изображение')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
     category_name = models.CharField(max_length=150, verbose_name='категория')
     buy_cost = models.IntegerField(verbose_name='стоимость покупки')
-    creation_date = models.DateTimeField(verbose_name='дата создания')
-    last_change_date = models.DateTimeField(verbose_name='дата последнего изменения')
+    creation_date = models.DateTimeField(verbose_name='дата создания', **NULLABLE)
+    last_change_date = models.DateTimeField(verbose_name='дата последнего изменения', **NULLABLE)
 
     def __str__(self):
         return f'{self.product_name} {self.category_name} {self.buy_cost}'
