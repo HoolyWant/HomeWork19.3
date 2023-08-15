9cad9d10079e9b04c5c9762e0a5ddfdababd62e9
hoolywant <sir.saltickow@yandex.ru>
2023-08-15 23:31:55 +0300

    created models for db

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='продукт')
+    description = models.CharField(max_length=150, verbose_name='описание')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение')
+    category_name = models.CharField(max_length=150, verbose_name='категория')
+    buy_cost = models.IntegerField(verbose_name='стоимость покупки')
+    creation_date = models.DateTimeField(verbose_name='дата создания')
+    last_change_date = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.product_name} {self.category_name} {self.buy_cost}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='категория')
+    description = models.CharField(max_length=150, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
