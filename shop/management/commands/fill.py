fa6bbfca971d98288b5e628da4a90e880b9586ef
hoolywant <sir.saltickow@yandex.ru>
2023-08-17 18:21:18 +0300

    so many changes for correctly work

--- shop/management/commands/fill.py
+++ shop/management/commands/fill.py
@@ -1,6 +1,6 @@
 from django.core.management import BaseCommand
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 class Command(BaseCommand):
@@ -9,22 +9,24 @@ class Command(BaseCommand):
         Product.objects.all().delete()
         product_list = [
             {'product_name': 'Шото', 'description': 'Ну вот такое оно да',
-             'category_name': 'Не знаю даже', 'buy_cost': '10000'},
+             'category_name': '1', 'buy_cost': '10000'},
             {'product_name': 'Ктото', 'description': 'Мы ему не доверяем',
-             'category_name': 'Чужой', 'buy_cost': '43545'},
+             'category_name': '1', 'buy_cost': '43545'},
             {'product_name': 'Непонятное', 'description': 'Не понимаем такое',
-             'category_name': 'Без комментариев', 'buy_cost': '5009'},
+             'category_name': '2', 'buy_cost': '5009'},
             {'product_name': 'Какоето', 'description': 'СТранное оно',
-             'category_name': 'Интересное', 'buy_cost': '399'},
+             'category_name': '2', 'buy_cost': '399'},
             {'product_name': 'Незнамашо', 'description': 'Ну тут не прибавить не отбавить',
-             'category_name': 'Шото', 'buy_cost': '10'},
+             'category_name': '1', 'buy_cost': '10'},
             {'product_name': 'Невиданное', 'description': 'Не видали раньше',
-             'category_name': '???', 'buy_cost': '1234000'},
+             'category_name': '1', 'buy_cost': '1234000'},
         ]
 
         list_for_push = []
         for product in product_list:
-            list_for_push.append(
-                Product(**product))
+            Product.objects.create(name=product['product_name'],
+                                   description=product['description'],
+                                   category_name=Category.objects.get(pk=product['category_name']),
+                                   buy_cost=product['buy_cost']
+                                   )
 
-        Product.objects.bulk_create(list_for_push)
